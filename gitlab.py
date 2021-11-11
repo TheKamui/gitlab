be7b3b54b2a92cb67c50066c06fff3e3f350ad7d
TheTheKamui <heizecarloss@gmail.com>
2021-11-11 12:42:59 +0700

    Add gear func

--- gitlab.py
+++ gitlab.py
@@ -24,6 +24,17 @@ class Cars(object):
         """
         self.eng = False
         return "%s braking" % self.carmark
+
+    def change_gear(self,n):
+        if n == "1" or n == "2" or n == "3" or n == "4":
+            self.gear = int(n)
+        elif n == "n":
+            self.gear = 0
+        elif n == "b":
+            self.gear = -1
+        else:
+            raise ValueError
+
     def drive(self):
         """
         Drive the car
